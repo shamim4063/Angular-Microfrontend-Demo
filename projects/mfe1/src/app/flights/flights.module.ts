2ee96f394d31d71032fc71afdf98d23d0dd55313
shamim4063 <shamim.vc@gmail.com>
2023-04-11 07:26:45 +0600

    Reformated

--- projects/mfe1/src/app/flights/flights.module.ts
+++ projects/mfe1/src/app/flights/flights.module.ts
@@ -4,9 +4,14 @@ import { FlightsSearchComponent } from './flights-search/flights-search.componen
 import { RouterModule } from '@angular/router';
 import { FLIGHTS_ROUTES } from './flights.routes';
 import { LazyComponent } from './lazy/lazy.component';
+import { EventLibModule } from 'event-lib';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(FLIGHTS_ROUTES)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(FLIGHTS_ROUTES),
+    EventLibModule,
+  ],
   declarations: [FlightsSearchComponent, LazyComponent],
 })
 export class FlightsModule {}
--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { APP_ROUTES } from './app.routes';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { EventLibModule } from 'event-lib';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(APP_ROUTES)
-  ],
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    NotFoundComponent
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES), EventLibModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
